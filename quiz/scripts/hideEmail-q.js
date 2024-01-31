c1a0f9a358171685085cf29c179610d664e46474
felix <fperez2020@outlook.com>
2024-01-31 16:27:06 -0500

    Daily activities

--- quiz/scripts/hideEmail-q.js
+++ quiz/scripts/hideEmail-q.js
@@ -6,6 +6,17 @@ window.onload = function hideEmail() {
     const list = document.getElementById('emails')
     list.innerHTML = '';
     for (const email of emails) {
-        // complete the loop
+        console.log(email);
+        console.log(isValid(email));
+
+        if (isValid(email)) {
+            const listItem = document.createElement('li');
+            listItem.textContent = email.slice(0, email.indexOf('@')).replace(/./g, '*') + email.slice(email.indexOf('@'));
+            list.appendChild(listItem);
+        }
+    }
+
+    function isValid(email) {
+        return /[a-zA-Z0-9_]+@(.+\.)*northeastern.edu$/.test(email);
     }
 }
